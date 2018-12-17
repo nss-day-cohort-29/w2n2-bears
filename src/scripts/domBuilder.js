74511308f7f1592077ea11093c7ac393a50a59e8
Grady Robbins <gradyrobbins13@gmail.com>
2018-12-17 15:25:54 -0600

    FOREACH object returned from the API call is added via innerHTML to the results div

--- src/scripts/domBuilder.js
+++ src/scripts/domBuilder.js
@@ -1,1 +1,25 @@
 // Append output
+// Called on click inside of each method (fetch) within data.js
+// Creates HTML elements with the item name, item information, and the type of item
+// resultType = class that's defined inside each fetch, WHICH ONLY RUNS ON CLICK "lets go!"
+const domBuilder = {
+
+
+    resultsBuilder(itemName, itemInfo, resultType){
+        let resultsField =
+        `
+        <div>
+            <div>
+                <p>
+                ${itemName}
+                <br />
+                ${itemInfo}</p>
+            </div>
+            <button type="button" class="saveButton ${resultType}">Save</button>
+         </div>
+         `
+
+         return resultsField;
+    }
+
+}
--- src/scripts/domComponents.js
+++ src/scripts/domComponents.js
@@ -1,1 +1,14 @@
 // Define output
+const domComponents = {
+    // Called initially when page is loaded
+    appendSearchInput(domElement){
+        let searchContainer = document.querySelector("#search_container");
+        searchContainer.innerHTML += domElement;
+    },
+
+    // Called when "lets go" is clicked, "appendResultsInput" is called
+    appendResultsInput(domElement){
+        let searchContainer = document.querySelector("#results_container");
+        searchContainer.innerHTML += domElement;
+    },
+}
