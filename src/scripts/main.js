2f0587ea8484fb8ba341189d1b4bb13d265c9ed0
Grady Robbins <gradyrobbins13@gmail.com>
2018-12-18 10:24:11 -0600

    able to capture user input in Restaurant search field

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,25 @@
 
 // data.getDiningInfo();
 
+
+
+let searchRestButton = document.getElementById("dine_search_btn");
+let userRestaurantSearchValue = "";
+
+
+searchRestButton.addEventListener("click", function(){
+    let searchInputValue = document.getElementById("dine_search_bar");
+    let userRestaurantSearchValue = searchInputValue.value;
+    console.log("userRestaurantSearchValue :" , userRestaurantSearchValue)
+
+    // console.log("Restaurants Button clicked!");
+
+
+});
+
+data.getDiningInfo(userRestaurantSearchValue);
+
 domComponents.appendResultsInput("bloop");
 domComponents.appendItineraryInput("bleep");
-
-
-
-// let resultsContainer = document.getElementById("results-container");
 
 // resultsContainer.innerHTML += domBuilder.resultsBuilder("Hattie B's", "hot chicken", "american");
